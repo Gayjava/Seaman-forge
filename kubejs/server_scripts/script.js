f35343eec82e55766ec3d97c440c44819970ae8c
Th3impasta <th3impasta300@gmail.com>
2023-06-19 22:15:14 -0400

    Version 2 complete

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -18,56 +18,6 @@ onEvent('recipes', event => {
 	event.remove({output: "haghogs_military_uniform:bulletproofvest_chestplate"})
 	event.remove({output: "haghogs_military_uniform:bulletproofvest_leggings"})
 	event.remove({output: "haghogs_military_uniform:bulletproofvest_boots"})
-
-	event.shaped('1x vs_clockwork:physics_infuser', [
-		'TET',
-		'EBE',
-		'TET'
-	  ], {
-		B: 'create_sa:brass_cube',
-		T: 'create:railway_casing',
-		E: 'create_sa:heap_of_experience'
-	})
-
-	event.shaped('1x vs_clockwork:physics_infuser', [
-		'CPC',
-		'SAS',
-		'CPC'
-	  ], {
-		C: 'create:copper_casing',
-		P: 'minecraft:piston',
-		S: 'create:shaft',
-		A: 'vs_clockwork:afterblazer'
-	})
-
-	event.shaped('1x vs_clockwork:bluperglue', [
-		'BS',
-		'NB'
-	  ], {
-		B: 'vs_clockwork:bluuguu',
-		S: 'create:iron_sheet',
-		N: 'minecraft:iron_nugget'
-	})
-
-	event.shaped('1x vs_clockwork:wing', [
-		'WS'
-	  ], {
-		W: 'create:sail_frame',
-		S: 'create:iron_sheet'
-	})
-
-	event.shaped('1x vs_clockwork:flap', [
-		'W'
-	  ], {
-		W: 'vs_clockwork:wing'
-	})
-
-	event.shaped('1x vs_clockwork:wing', [
-		'W'
-	  ], {
-		W: 'vs_clockwork:flap'
-	})
-
 })
 
 onEvent('item.tags', event => {
